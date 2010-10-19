6f5f07aed5db08d7e314d8fd3ec11c0cd00b9c97
Brian Quinn <briandquinn@gmail.com>
2010-10-19 09:37:44 +0100

    Fixed issue with order.finalize! override

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,14 +1,13 @@
 Order.class_eval do
   # Finalizes an in progress order after checkout is complete.
   # Called after transition to complete state when payments will have been processed
-  def finalize!
-    self.out_of_stock_items = InventoryUnit.sell_units(self) if Spree::Config[:track_inventory_levels]
-    update_attribute(:completed_at, Time.now)
-    # lock any optional adjustments (coupon promotions, etc.)
-    adjustments.optional.each { |adjustment| adjustment.update_attribute("locked", true) }
-    OrderMailer.confirm_email(self).deliver
+  def finalize_with_gift_card!
+    debugger
+    finalize_without_gift_card!
     self.line_items.each do |li|
       OrderMailer.gift_card_email(li.gift_card).deliver if li.gift_card
     end
   end
+  alias_method_chain :finalize!, :gift_card
+
 end
