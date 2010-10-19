23ecb018c854f5948dac3934afa51720b6adbe3d
Brian Quinn <briandquinn@gmail.com>
2010-10-19 09:40:07 +0100

    Removed debugger statement

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -2,7 +2,6 @@ Order.class_eval do
   # Finalizes an in progress order after checkout is complete.
   # Called after transition to complete state when payments will have been processed
   def finalize_with_gift_card!
-    debugger
     finalize_without_gift_card!
     self.line_items.each do |li|
       OrderMailer.gift_card_email(li.gift_card).deliver if li.gift_card
